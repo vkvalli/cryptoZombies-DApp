6c5117b858dd767a8fb09b3a495bab919b5398f5
VK Valli <vk.valli2909@gmail.com>
2025-10-21 05:41:24 -0700

    final changes made. added kittycontract.sol from kittyfeeding and modified deply_migrations from the same

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,68 @@
-var safemath = artifacts.require("./safemath.sol");
-var zombiefactory = artifacts.require("./zombiefactory.sol");
-var zombiefeeding = artifacts.require("./zombiefeeding.sol");
-var zombiehelper = artifacts.require("./zombiehelper.sol");
-var zombieattack = artifacts.require("./zombieattack.sol");
-var zombieownership = artifacts.require("./zombieownership.sol");
+// var safemath = artifacts.require("./safemath.sol");
+// var zombiefactory = artifacts.require("./zombiefactory.sol");
+// var zombiefeeding = artifacts.require("./zombiefeeding.sol");
+// var zombiehelper = artifacts.require("./zombiehelper.sol");
+// var zombieattack = artifacts.require("./zombieattack.sol");
+// var zombieownership = artifacts.require("./zombieownership.sol");
 
-module.exports = function(deployer) {
+// module.exports = function(deployer) {
 
-    deployer.deploy(safemath);
-    deployer.deploy(zombiefactory);
-    deployer.deploy(zombiefeeding);
-    deployer.deploy(zombiehelper);
-    deployer.deploy(zombieattack);
-    deployer.deploy(zombieownership);
-}
+//     deployer.deploy(safemath);
+//     deployer.deploy(zombiefactory);
+//     deployer.deploy(zombiefeeding);
+//     deployer.deploy(zombiehelper);
+//     deployer.deploy(zombieattack);
+//     deployer.deploy(zombieownership);
+// }
+
+
+// const safemath = artifacts.require("./safemath.sol");
+// const kittycontact = artifacts.require("./kittycontract.sol");
+// const zombiefactory = artifacts.require("./zombiefactory.sol");
+// const zombiefeeding = artifacts.require("./zombiefeeding.sol");
+// const zombiehelper = artifacts.require("./zombiehelper.sol");
+// const zombieattack = artifacts.require("./zombieattack.sol");
+// const zombieownership = artifacts.require("./zombieownership.sol");
+
+// module.exports = async function(deployer) {
+
+//     await deployer.deploy(safemath);
+//     await deployer.deploy(kittycontact);
+//     const kittyInstance = await kittycontact.deployed();
+
+//     await deployer.deploy(zombiefactory);
+//     await deployer.deploy(zombiefeeding);
+//     const feedingInstance = await zombiefeeding.deployed();
+//     await feedingInstance.setKittyContractAddress(kittyInstance.address);
+
+//     await deployer.deploy(zombiehelper);
+//     await deployer.deploy(zombieattack);
+//     await deployer.deploy(zombieownership);
+
+//     console.log("KittyContract deployed at:", kittyInstance.address);
+//     console.log("ZombieOwnership deployed at:", feedingInstance.address);
+// }
+
+const SafeMath = artifacts.require("safemath.sol");
+const KittyContract = artifacts.require("kittycontract.sol");
+const ZombieOwnership = artifacts.require("zombieownership.sol");
+
+module.exports = async function (deployer) {
+  //  Deploy SafeMath
+  await deployer.deploy(SafeMath);
+
+  //  Deploy KittyContract
+  await deployer.deploy(KittyContract);
+  const kitty = await KittyContract.deployed();
+
+  // Deploy ZombieOwnership (this includes all inherited zombie contracts)
+  await deployer.deploy(ZombieOwnership);
+  const zombies = await ZombieOwnership.deployed();
+
+  // Link KittyContract to ZombieOwnership
+  await zombies.setKittyContractAddress(kitty.address);
+
+
+  console.log("KittyContract deployed at:", kitty.address);
+  console.log("ZombieOwnership deployed at:", zombies.address);
+};
